74d91cae2148b2d8266ba884f7c82f1be8b9fbdf
Iuumi <167376541+Iuumi@users.noreply.github.com>
2025-12-01 18:59:43 -0500

    added general instructions into final file

--- final_project.py
+++ final_project.py
@@ -1,2 +1,9 @@
 # Template for Final Project
 from CPU import CpuPlayer
+
+#parse args here
+
+if __name__ == "__main__":
+    pass
+    #define player classes with arg info
+    # call game method with players as input
