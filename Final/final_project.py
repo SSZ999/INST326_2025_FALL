f04a71078813617d1203e639b2ebc109ef27d5b9
Seniah Zachariah <szachar3@terpmail.umd.edu>
2025-12-01 19:50:06 -0500

    adding parse args

--- Final/final_project.py
+++ Final/final_project.py
@@ -1,7 +1,26 @@
 # Template for Final Project
 from Final.CPU import CpuPlayer
 
-#parse args here
+
+
+def parse_args(arglist):
+
+   """Process command line arguments. 
+
+   Args: 
+       arglist (list of str): arguments from the command line.
+
+   Returns: 
+
+       namespace: the parsed arguments, as a namespace."""
+
+   parser = argparse.ArgumentParser()
+
+   parser.add_argument("player_name", help = "provide players name")
+   parser.add_argument("computer_players", type = int, default = 0, help = "provide an optional integer value representing number of computer players")
+   
+   return parser.parse_args(arglist)
+
 
 if __name__ == "__main__":
     pass
